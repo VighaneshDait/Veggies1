79088a7273a9950832110460d7113c274cf0a36e
VighaneshDait <vighaneshdait06@gmail.com>
2017-04-14 17:34:52 +0530

    myfirstcommit

---
+++ veggiesBackEnd/src/main/java/com/vighanesh/veggiesbackend/App.java
@@ -0,0 +1,13 @@
+package com.vighanesh.veggiesbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
